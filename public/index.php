7c6665b8e446fc3dae8fbc2a3dbc1247259b9f4d
Gutergues Gomes <gutergues@gmail.com>
2026-04-21 19:33:12 -0300

    Autoload Config

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ error_reporting(E_ALL);
 
 require_once('../app/core/Router.php');
 
+require_once(__DIR__ . "/../vendor/autoload.php");
+
 //use app\core\Router;
 
 // Obtém a URL que foi passada pelo .htaccess
